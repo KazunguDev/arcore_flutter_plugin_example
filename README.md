a30b50d1aeca532996c22ffda90b64a9ef523c8e
KazunguDev <kazungudev@gmail.com>
2023-09-15 15:53:58 +0300

    AR flutter

---
+++ README.md
@@ -0,0 +1,7 @@
+# arcore_flutter_plugin_example
+
+Demonstrates how to use the arcore_flutter_plugin plugin and develop AR Flutter Android Apps.
+
+## Screenshots
+
+
---
+++ android/app/src/main/kotlin/com/kazungudev/arcore_flutter_plugin_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kazungudev.arcore_flutter_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
